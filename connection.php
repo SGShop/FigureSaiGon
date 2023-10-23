e08222c14d35deac1ee28e38514cdf8e22978e09
Tăng Minh Nhựt <nhuttmgcc200149@fpt.edu.vn>
2023-10-23 11:18:07 +0700

    Update connection database

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost','root','','online_shopping')
+$conn = mysqli_connect('i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','jyhet75vo1q4s63g','xlqaph3muousl5ql','zdwcc5jw66ujolvq')
  or die("Can not connect database".mysqli_connect_error());
 ?>
