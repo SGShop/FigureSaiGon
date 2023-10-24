3a7089745c46a971d684b3b6f09b0fb333513bd6
Tăng Minh Nhựt <nhuttmgcc200149@fpt.edu.vn>
2023-10-24 07:55:42 +0700

    update db

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','jyhet75vo1q4s63g','xlqaph3muousl5ql','zdwcc5jw66ujolvq')
+$conn = mysqli_connect('frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','uw67e2q7hlto4xto','t9wcoac26lyu5evw','vv2rr2gx02thsgs7')
  or die("Can not connect database".mysqli_connect_error());
 ?>
